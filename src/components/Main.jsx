55e71d8016c2c7033b8341ab111479c7e8372d42
simone-scotto <simone.scottotkd@gmail.com>
2025-07-14 17:56:51 +0200

    services lists

---
+++ src/components/Main.jsx
@@ -0,0 +1,52 @@
+export default function Main () {
+    return (
+        <main>
+            <section className="content bg-black">
+                <div className="container ">  -- Content goes Here -- </div>
+
+            </section>
+            <section className="services-list bg-primary">
+                <div className="container">
+                    <ul className="services-list">
+                        <li>
+                            <a className="services-list-items" href="#">
+                                <div>
+                                    <img src="/img/buy-comics-digital-comics.png" alt="nuy comics" />
+                                </div>
+                                digital comics
+                            </a>
+                        </li>
+                        <li>
+                            <a className="services-list-items" href="#">
+                                <div><img src="/img/buy-comics-merchandise.png" alt="nuy comics" /></div>
+                            
+                            dc merchandise
+                            </a>
+                        </li>
+                        <li>
+                            <a className="services-list-items" href="#">
+                                <div><img src="/img/buy-comics-subscriptions.png" alt="nuy comics" /></div>
+                            
+                            subscription</a>
+                        </li>
+                        <li>
+                            <a className="services-list-items" href="#">
+                                <div> <img src="/img/buy-comics-shop-locator.png" alt="nuy comics" /></div>
+                           
+                            comic shop locator</a>
+                            </li>
+                        <li>
+                            <a className="services-list-items" href="#">
+                                <div> <img src="/img/buy-dc-power-visa.svg" alt="nuy comics" /></div>
+                           
+                            dc power visa</a>
+                            </li>
+                    </ul>
+                </div>
+                
+            </section>
+        
+        </main>
+        
+    )
+} 
