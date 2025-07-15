7f66628cf735cd6849bbda28330ca57b077f6537
simone-scotto <simone.scottotkd@gmail.com>
2025-07-15 16:38:53 +0200

    main cards

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,49 +1,117 @@
+const services = [{
+    src:"/img/buy-comics-digital-comics.png",
+    title:  "digital comics"
+}
+,{
+    src: `/img/buy-comics-merchandise.png`,
+    title:  "dc merchandise"
+},{
+    src:"/img/buy-comics-shop-locator.png",
+    title:  "subscription"
+},{
+    src:"/img/buy-comics-subscriptions.png",
+    title:  "digital comics"
+},{
+    src:"/img/buy-dc-power-visa.svg",
+    title:  "comic shop locator"
+}]
+
+
+import comics from "../db/comics"
+
+
+
+
+
 export default function Main () {
     return (
         <main>
-            <section className="content bg-black">
-                <div className="container ">  -- Content goes Here -- </div>
+            <section className="jumbo ">
 
             </section>
-            <section className="services-list bg-primary">
+            <section className="comics container">
+                <div className="row row-cols-6 g-4">
+                    {comics.map((comic) => (
+                        
+                            <div className="comic-card">
+                                <div className="comic-card-head">
+                                    <img className="img-fluid" src={comic.thumb} alt="comic"/></div>
+                                <div className="comic-card-body">{comic.series}</div>
+                            </div>
+                            
+                            
+                        
+                        ))}
+                    
+                </div>
+            </section>
+            <section className="bg-primary">
                 <div className="container">
                     <ul className="services-list">
-                        <li>
-                            <a className="services-list-items" href="#">
-                                <div>
-                                    <img src="/img/buy-comics-digital-comics.png" alt="nuy comics" />
-                                </div>
-                                digital comics
-                            </a>
-                        </li>
-                        <li>
-                            <a className="services-list-items" href="#">
-                                <div><img src="/img/buy-comics-merchandise.png" alt="nuy comics" /></div>
-                            
-                            dc merchandise
-                            </a>
-                        </li>
-                        <li>
-                            <a className="services-list-items" href="#">
-                                <div><img src="/img/buy-comics-subscriptions.png" alt="nuy comics" /></div>
-                            
-                            subscription</a>
-                        </li>
-                        <li>
-                            <a className="services-list-items" href="#">
-                                <div> <img src="/img/buy-comics-shop-locator.png" alt="nuy comics" /></div>
-                           
-                            comic shop locator</a>
-                            </li>
-                        <li>
-                            <a className="services-list-items" href="#">
-                                <div> <img src="/img/buy-dc-power-visa.svg" alt="nuy comics" /></div>
-                           
-                            dc power visa</a>
-                            </li>
-                    </ul>
+                        
+
+                            {services.map((service) => (
+                                <li className="services-list-items"><a href="#">
+                                   <img src={service.src} alt="#" /> 
+                                    {service.title}
+                                </a>
+
+                                </li>
+                            ))}
+                        </ul>                    
+                    
                 </div>
-                
+            </section>
+            <section className="lists-section ">
+                <div className="container">
+
+               
+                    <div className="closing-lists text-light">
+                        <ul>
+                            <li><h3>dc comics</h3></li>
+                            <li>Characters</li>
+                            <li>Comics</li>
+                            <li>Movies</li>
+                            <li>TV</li>
+                            <li>Games</li>
+                            <li>Videos</li>
+                            <li>news</li>
+                        </ul>
+                        <ul>
+                            <li><h3>shop</h3></li>
+                            <li>Shop DC</li>   
+                            <li>Shop DC Collectibles</li>                   
+                        </ul>
+                        <ul>
+                            <li><h3>dc</h3></li>
+                            <li>Terms Of Use</li>
+                            <li>Privacy Policy (New)</li>
+                            <li>Ad Choiices</li>
+                            <li>Advertising</li>
+                            <li>Jobs</li>
+                            <li>Subscriptions</li>
+                            <li>Talent Workshops</li>
+                            <li>CPSC Certificates</li>
+                            <li>Ratings</li>
+                            <li>Shop Help</li>
+                            <li>Contact Us</li>
+                        </ul>
+                        <ul>
+                            <li><h3>sites</h3></li>
+                            <li>DC</li>
+                            <li>MAD Magazine</li>
+                            <li>DC Kids</li>
+                            <li>DC Universe</li> 
+                            <li>DC Power Visa</li>                         
+                        </ul>
+                    </div>
+                    <div className="dc-backbround">
+                        <img src="./img/dc-logo-bg-png" alt="sddc" />
+                        
+                    </div>
+                </div>
+
+              
             </section>
         
         </main>
